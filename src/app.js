4c99f77082c87055e42fae23591318a10aa44e4d
improperboy <divyanshuomar856@gmail.com>
2026-04-17 14:26:46 +0530

    error testing in cicd

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express(
 
 app.use(express.json());
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'UP', message: 'Server is healthy' });
 });
-app
+
 module.exports = app;
