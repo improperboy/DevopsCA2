6783293d654e767ff9e97491f0947096831f48f6
improperboy <divyanshuomar856@gmail.com>
2026-04-17 14:31:13 +0530

    testing 3

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express(
+const express = require('express');
+const app = express();
 
 app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is testing of CI/CD 🚀');
+});
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'UP', message: 'Server is healthy' });
